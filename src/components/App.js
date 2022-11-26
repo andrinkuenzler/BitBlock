ee9fc29f0a7431592bda4f0aece76f08783daf4d
Michael Tellenbach <michael.tellenbach@stud.hslu.ch>
2022-11-26 12:03:50 +0100

    Restrukturierung

--- src/components/App.js
+++ src/components/App.js
@@ -3,60 +3,70 @@ import logo from '../logo.png';
 import './App.css';
 
 class App extends Component {
-    function getIndex() {
-        var file = document.getElementById("fileForUpload").files[0];
-        if (file) {
-            var reader = new FileReader();
-            reader.readAsText(file, "UTF-8");
-            reader.onload = function (evt) {
-                document.getElementById("fileContents").innerHTML = evt.target.result;
-            }
-            reader.onerror = function (evt) {
-                document.getElementById("fileContents").innerHTML = "error reading file";
-            }
-        }
-    }
   render() {
     return (
-      <div>
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <a
-            className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Dapp University
-          </a>
-        </nav>
-        <div className="container-fluid mt-5">
-          <div className="row">
-            <main role="main" className="col-lg-12 d-flex text-center">
-              <div className="content mr-auto ml-auto">
-                <a
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={logo} className="App-logo" alt="logo" />
-                </a>
-                <h1>Dapp University Starter Kit</h1>
-                <p>
-                  Edit <code>src/components/App.js</code> and save to reload.
-                </p>
-                <a
-                  className="App-link"
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  LEARN BLOCKCHAIN <u><b>NOW! </b></u>
-                </a>
-              </div>
-            </main>
-          </div>
-        </div>
-      </div>
+        <section class="w3-container">
+            <h3 id="formular">Decentralized Ticket Platform</h3>
+            <form class="w3-panel w3-col m3 w3-card w3-row" onsubmit="" action="" method="">
+                <h4>Create Event</h4>
+
+                <label for="eventNameCreate">Event name:</label><br />
+                <input name="eventNameCreate" type="text" size="20" value="" required /><br />
+                <br />
+
+                <label for="ticketQuantityCreate">Number of tickets:</label><br />
+                <input name="ticketQuantityCreate" type="number" size="4" min="1" max="100000" value="" required /><br />
+                <br />
+
+                <label for="ticketPrice">Price per ticket:</label><br />
+                <input name="ticketPrice" type="number" size="4" min="10" max="500" value="" required /><br />
+                <br />
+
+                <button class="w3-button w3-light-grey" type="submit" name="send">Send</button>
+                <button class="w3-button w3-light-grey" type="reset" name="delete">Delete</button>
+            </form>
+
+            <form class="w3-panel w3-col m3 w3-card w3-row" onsubmit="" action="" method="">
+                <h4>Buy Ticket</h4>
+
+                <label for="eventNameBuy">Event name:</label><br />
+                <input name="eventNameBuy" type="text" size="20" value="" required /><br />
+                <br />
+
+                <label for="ticketQuantityBuy">Number of tickets:</label><br />
+                <input name="ticketQuantityBuy" type="number" size="4" min="1" max="1" value="1" required /><br />
+                <br />
+
+                <button class="w3-button w3-light-grey" type="submit" name="send">Send</button>
+                <button class="w3-button w3-light-grey" type="reset" name="delete">Delete</button>
+            </form>
+
+            <form class="w3-panel w3-col m3 w3-card w3-row" onsubmit="" action="" method="">
+                <h4>Get Entry</h4>
+
+                <label for="eventNameEntry">Event name:</label><br />
+                <input name="eventNameEntry" type="text" size="20" value="" required /><br />
+                <br />
+
+                <button class="w3-button w3-light-grey" type="submit" name="send">Send</button>
+                <button class="w3-button w3-light-grey" type="reset" name="delete">Delete</button>
+            </form>
+
+            <form class="w3-panel w3-col m3 w3-card w3-row" onsubmit="" action="" method="">
+                <h4>Refund Ticket</h4>
+
+                <label for="eventNameRefund">Event name:</label><br />
+                <input name="eventNameRefund" type="text" size="20" value="" required /><br />
+                <br />
+
+                <label for="ticketQuantityRefund">Number of tickets:</label><br />
+                <input name="ticketQuantityRefund" type="number" size="4" min="1" max="1" value="1" required /><br />
+                <br />
+
+                <button class="w3-button w3-light-grey" type="submit" name="send">Send</button>
+                <button class="w3-button w3-light-grey" type="reset" name="delete">Delete</button>
+            </form>
+        </section>
     );
   }
 }
